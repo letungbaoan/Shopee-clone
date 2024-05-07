71e3eb347e692f7477991169292ab2f9488f9a39
letungbaoan <baoan7604@gmail.com>
2024-05-07 16:00:55 +0700

    Custom class .container using plugin Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  corePlugins: {
+    container: false
+  },
   theme: {
     extend: {
       colors: {
@@ -8,5 +13,17 @@ module.exports = {
       }
     }
   },
-  plugins: []
+  plugins: [
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        '.container': {
+          maxWidth: '80rem',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          paddingLeft: '1rem',
+          paddingRight: '1rem'
+        }
+      })
+    })
+  ]
 }

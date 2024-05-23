914b17d8f979cf0e4c67942269b073daa89856e6
letungbaoan <baoan7604@gmail.com>
2024-05-23 09:55:43 +0700

    Code UI AsideFilter

--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,27 +1,37 @@
-import React from 'react'
+import React, { InputHTMLAttributes } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
-  placeholder?: string
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  className?: string
-  name: string
-  register: UseFormRegister<any>
-  autoComplete?: string
+  classNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
+  rules?: RegisterOptions
 }
 
-export default function Input({ type, placeholder, name, register, className, autoComplete, errorMessage }: Props) {
+export default function Input({
+  type,
+  placeholder,
+  name,
+  register,
+  className,
+  autoComplete,
+  errorMessage,
+  rules,
+  classNameInput = 'p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm',
+  classNameError='mt-1 text-red-600 min-h-[1.25rem] text-sm text-left'
+}: Props) {
+  const registerResult = register && name ? {...register(name, rules)} : {}
   return (
     <div className={className}>
       <input
         type={type}
-        className='p-3 w-full outline-none border border-gray-300 focus:border-gray-500 rounded-sm focus:shadow-sm'
+        className={classNameInput}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        {...register(name)}
+        {...registerResult}
       />
-      <div className='mt-1 text-red-600 min-h-[1.25rem] text-sm text-left'>{errorMessage}</div>
+      <div className={classNameError}>{errorMessage}</div>
     </div>
   )
 }
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -1,4 +1,24 @@
+import AsideFilter from "./AsideFilter";
+import Product from "./Product/Product";
+import SortProductList from "./SortProductList";
 
 export default function ProductList() {
-  return <div>ProductList</div>
+  return <div className="bg-gray-200 py-6">
+    <div className="container">
+      <div className="grid grid-cols-12 gap-6">
+        <div className="col-span-3">
+          <AsideFilter/>
+        </div>
+        <div className="col-span-9">
+          <SortProductList/>
+          <div className="mt-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
+            {Array(30)
+            .fill(0).map((_,index) => (
+              <Product key={index}/>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 }

c9c44d8727267d16fc48774717c4b4404699efa3
letungbaoan <baoan7604@gmail.com>
2024-05-23 14:39:27 +0700

    Code UI Product component

--- src/pages/ProductList/Product/Product.tsx
+++ src/pages/ProductList/Product/Product.tsx
@@ -1,7 +1,72 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Product() {
   return (
-    <div>Product</div>
+    <Link to='/'>
+      <div className='bg-white shadow rounded-sm hover:translate-y-[-0.04rem] hover:shadow-md duration-100 transition-transform overflow-hidden'>
+        <div className='w-full pt-[100%] relative'>
+          <img
+            src='https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-litlkyky6kuk01_tn'
+            alt=''
+            className='absolute top-0 left-0 bg-white w-full h-full object-cover'
+          />
+        </div>
+        <div className='p-2 overflow-hidden'>
+          <div className='min-h-[2rem] line-clamp-2 text-xs'>
+            Quần đùi nam nữ unisex in hình vịt vàng chất liệu umi dày dặn form đẹp thoáng mát - REW2021
+          </div>
+          <div className='flex items-center mt-3'>
+            <div className='line-through max-w-[50%] text-gray-500 truncate'>
+              <span className='text-xs'>đ</span>
+              <span>99.000</span>
+            </div>
+            <div className='text-orange truncate ml-1'>
+              <span className='text-xs'>đ</span>
+              <span>65.000</span>
+            </div>
+          </div>
+          <div className='mt-3 flex items-center justify-start'>
+            <div className='flex items-center'>
+              <div className='relative'>
+                <div className='absolute top-0 left-0 h-full overflow-hidden' style={{ width: '50%' }}>
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
+                    className='w-3 h-3 fill-yellow-300 text-yellow-300'
+                  >
+                    <polygon
+                      points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeMiterlimit={10}
+                    />
+                  </svg>
+                </div>
+                <svg
+                  enableBackground='new 0 0 15 15'
+                  viewBox='0 0 15 15'
+                  x={0}
+                  y={0}
+                  className='w-3 h-3 fill-current text-gray-300'
+                >
+                  <polygon
+                    points='7.5 .8 9.7 5.4 14.5 5.9 10.7 9.1 11.8 14.2 7.5 11.6 3.2 14.2 4.3 9.1 .5 5.9 5.3 5.4'
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeMiterlimit={10}
+                  />
+                </svg>
+              </div>
+            </div>
+            <div className='ml-2 text-sm'>
+              <span className='ml-1'>Đã bán </span>
+              <span>5.66k</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
           paddingRight: '1rem'
         }
       })
-    })
+    }),
+    require('@tailwindcss/line-clamp')
   ]
 }

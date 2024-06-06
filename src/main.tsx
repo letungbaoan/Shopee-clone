ce8b5be933d45ca1c9889bdc3f3843583e35e1e5
letungbaoan <baoan7604@gmail.com>
2024-06-06 16:14:25 +0700

    Add item to cart

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from 'src/App'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { AppProvider } from './contexts/app.context'
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       refetchOnWindowFocus: false
@@ -15,7 +15,7 @@ const queryClient = new QueryClient({
   }
 })
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,13 +1,17 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import DOMPurify from 'dompurify'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import productApi from 'src/apis/product.api'
+import purchaseApi from 'src/apis/purchase.api'
+import { toast } from 'react-toastify'
 import ProductRating from 'src/components/ProductRating'
+import QuantityController from 'src/components/QuantityController'
+import { purchasesStatus } from 'src/constants/purchase'
+import { queryClient } from 'src/main'
 import { Product as ProductType, ProductListConfig } from 'src/types/product.type'
 import { formatCurrency, formatNumberToSocialStyle, getIdFromNameId, rateSale } from 'src/utils/utils'
 import Product from '../ProductList/components/Product'
-import QuantityController from 'src/components/QuantityController'
 
 export default function ProductDetail() {
   const [buyCount, setBuyCount] = useState(1)
@@ -17,7 +21,6 @@ export default function ProductDetail() {
     queryKey: ['product', id],
     queryFn: () => productApi.getProductDetail(id as string)
   })
-
   const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
   const [activeImage, setActiveImage] = useState('')
   const product = productDetailData?.data.data
@@ -26,7 +29,6 @@ export default function ProductDetail() {
     () => (product ? product.images.slice(...currentIndexImages) : []),
     [product, currentIndexImages]
   )
-
   const queryConfig: ProductListConfig = { limit: '20', page: '1', category: product?.category._id }
 
   const { data: productsData } = useQuery({
@@ -34,8 +36,11 @@ export default function ProductDetail() {
     queryFn: () => {
       return productApi.getProducts(queryConfig)
     },
-    enabled: Boolean(product),
-    staleTime: 3 * 60 * 1000
+    staleTime: 3 * 60 * 1000,
+    enabled: Boolean(product)
+  })
+  const addToCartMutation = useMutation({
+    mutationFn: (body: { product_id: string; buy_count: number }) => purchaseApi.addToCart(body)
   })
 
   useEffect(() => {
@@ -44,19 +49,19 @@ export default function ProductDetail() {
     }
   }, [product])
 
-  const prev = () => {
-    if (currentIndexImages[0] > 0) {
-      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
-    }
-  }
-
   const next = () => {
     if (currentIndexImages[1] < (product as ProductType).images.length) {
       setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
     }
   }
 
-  const chooseActiveImage = (img: string) => {
+  const prev = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
+  const chooseActive = (img: string) => {
     setActiveImage(img)
   }
 
@@ -64,7 +69,10 @@ export default function ProductDetail() {
     const rect = event.currentTarget.getBoundingClientRect()
     const image = imageRef.current as HTMLImageElement
     const { naturalHeight, naturalWidth } = image
-    const { offsetX, offsetY } = event.nativeEvent
+
+    const offsetX = event.pageX - (rect.x + window.scrollX)
+    const offsetY = event.pageY - (rect.y + window.scrollY)
+
     const top = offsetY * (1 - naturalHeight / rect.height)
     const left = offsetX * (1 - naturalWidth / rect.width)
     image.style.width = naturalWidth + 'px'
@@ -82,6 +90,18 @@ export default function ProductDetail() {
     setBuyCount(value)
   }
 
+  const addToCart = () => {
+    addToCartMutation.mutate(
+      { buy_count: buyCount, product_id: product?._id as string },
+      {
+        onSuccess: (data) => {
+          toast.success(data.data.message, { autoClose: 1000 })
+          queryClient.invalidateQueries({ queryKey: ['purchases', { status: purchasesStatus.inCart }] })
+        }
+      }
+    )
+  }
+
   if (!product) return null
   return (
     <div className='bg-gray-200 py-6'>
@@ -91,20 +111,20 @@ export default function ProductDetail() {
             <div className='col-span-5'>
               <div
                 className='relative w-full cursor-zoom-in overflow-hidden pt-[100%] shadow'
-                onMouseMove={(event) => handleZoom(event)}
+                onMouseMove={handleZoom}
                 onMouseLeave={handleRemoveZoom}
               >
                 <img
                   src={activeImage}
                   alt={product.name}
-                  className='pointer-events-none absolute left-0 top-0 h-full w-full bg-white object-cover'
+                  className=' absolute left-0 top-0 h-full w-full bg-white object-cover'
                   ref={imageRef}
                 />
               </div>
-              <div className='relative grid grid-cols-5 gap-1'>
+              <div className='relative mt-4 grid grid-cols-5 gap-1'>
                 <button
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
                   onClick={prev}
-                  className='absolute  left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
                 >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -114,25 +134,25 @@ export default function ProductDetail() {
                     stroke='currentColor'
                     className='h-5 w-5'
                   >
-                    <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5 8.25 12l7.5-7.5' />
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
                   </svg>
                 </button>
                 {currentImages.map((img) => {
                   const isActive = img === activeImage
                   return (
-                    <div className='relative w-full pt-[100%]' key={img} onMouseEnter={() => chooseActiveImage(img)}>
+                    <div className='relative w-full pt-[100%]' key={img} onMouseEnter={() => chooseActive(img)}>
                       <img
                         src={img}
                         alt={product.name}
                         className='absolute left-0 top-0 h-full w-full cursor-pointer bg-white object-cover'
                       />
-                      {isActive && <div className='absolute inset-0 border-2 border-orange'></div>}
+                      {isActive && <div className='absolute inset-0 border-2 border-orange' />}
                     </div>
                   )
                 })}
                 <button
-                  onClick={next}
                   className='absolute right-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={next}
                 >
                   <svg
                     xmlns='http://www.w3.org/2000/svg'
@@ -142,7 +162,7 @@ export default function ProductDetail() {
                     stroke='currentColor'
                     className='h-5 w-5'
                   >
-                    <path strokeLinecap='round' strokeLinejoin='round' d='m8.25 4.5 7.5 7.5-7.5 7.5' />
+                    <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
                   </svg>
                 </button>
               </div>
@@ -151,22 +171,22 @@ export default function ProductDetail() {
               <h1 className='text-xl font-medium uppercase'>{product.name}</h1>
               <div className='mt-8 flex items-center'>
                 <div className='flex items-center'>
-                  <span className='mr-1 border-b border-orange text-orange'>{product.rating}</span>
+                  <span className='mr-1 border-b border-b-orange text-orange'>{product.rating}</span>
                   <ProductRating
                     rating={product.rating}
                     activeClassname='fill-orange text-orange h-4 w-4'
                     nonActiveClassname='fill-gray-300 text-gray-300 h-4 w-4'
                   />
                 </div>
-                <div className='mx-4 h-4 w-[1px] bg-gray-300' />
+                <div className='mx-4 h-4 w-[1px] bg-gray-300'></div>
                 <div>
                   <span>{formatNumberToSocialStyle(product.sold)}</span>
-                  <span className='ml-1 text-gray-500'>Đã Bán</span>
+                  <span className='ml-1 text-gray-500'>Đã bán</span>
                 </div>
               </div>
               <div className='mt-8 flex items-center bg-gray-50 px-5 py-4'>
-                <div className='text-gray-500 line-through'>đ{formatCurrency(product.price_before_discount)}</div>
-                <div className='ml-3 text-3xl font-medium text-orange'>đ{formatCurrency(product.price)}</div>
+                <div className='text-gray-500 line-through'>₫{formatCurrency(product.price_before_discount)}</div>
+                <div className='ml-3 text-3xl font-medium text-orange'>₫{formatCurrency(product.price)}</div>
                 <div className='ml-4 rounded-sm bg-orange px-1 py-[2px] text-xs font-semibold uppercase text-white'>
                   {rateSale(product.price_before_discount, product.price)} giảm
                 </div>
@@ -183,15 +203,36 @@ export default function ProductDetail() {
                 <div className='ml-6 text-sm text-gray-500'>{product.quantity} sản phẩm có sẵn</div>
               </div>
               <div className='mt-8 flex items-center'>
-                <button className='flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/20 px-5 capitalize text-orange shadow-sm hover:bg-orange/5'>
-                  <img
-                    alt='icon-add-to-cart'
+                <button
+                  onClick={addToCart}
+                  className='flex h-12 items-center justify-center rounded-sm border border-orange bg-orange/10 px-5 capitalize text-orange shadow-sm hover:bg-orange/5'
+                >
+                  <svg
+                    enableBackground='new 0 0 15 15'
+                    viewBox='0 0 15 15'
+                    x={0}
+                    y={0}
                     className='mr-[10px] h-5 w-5 fill-current stroke-orange text-orange'
-                    src='https://deo.shopeemobile.com/shopee/shopee-pcmall-live-sg/productdetailspage/0f3bf6e431b6694a9aac.svg'
-                  />
+                  >
+                    <g>
+                      <g>
+                        <polyline
+                          fill='none'
+                          points='.5 .5 2.7 .5 5.2 11 12.4 11 14.5 3.5 3.7 3.5'
+                          strokeLinecap='round'
+                          strokeLinejoin='round'
+                          strokeMiterlimit={10}
+                        />
+                        <circle cx={6} cy='13.5' r={1} stroke='none' />
+                        <circle cx='11.5' cy='13.5' r={1} stroke='none' />
+                      </g>
+                      <line fill='none' strokeLinecap='round' strokeMiterlimit={10} x1='7.5' x2='10.5' y1={7} y2={7} />
+                      <line fill='none' strokeLinecap='round' strokeMiterlimit={10} x1={9} x2={9} y1='8.5' y2='5.5' />
+                    </g>
+                  </svg>
                   Thêm vào giỏ hàng
                 </button>
-                <button className='ml-4 flex h-12 min-w-[5rem] items-center justify-center rounded-sm bg-orange px-16 capitalize  text-white shadow-sm outline-none hover:bg-orange/90'>
+                <button className='fkex ml-4 h-12 min-w-[5rem] items-center justify-center rounded-sm bg-orange px-5 capitalize text-white shadow-sm outline-none hover:bg-orange/90'>
                   Mua ngay
                 </button>
               </div>
@@ -201,7 +242,7 @@ export default function ProductDetail() {
       </div>
       <div className='mt-8'>
         <div className='container'>
-          <div className='bg-white p-4 shadow'>
+          <div className=' bg-white p-4 shadow'>
             <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>Mô tả sản phẩm</div>
             <div className='mx-4 mb-4 mt-12 text-sm leading-loose'>
               <div
@@ -213,6 +254,7 @@ export default function ProductDetail() {
           </div>
         </div>
       </div>
+
       <div className='mt-8'>
         <div className='container'>
           <div className='uppercase text-gray-400'>CÓ THỂ BẠN CŨNG THÍCH</div>

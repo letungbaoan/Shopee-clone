cd2d8ed646a8f354466e82ff244c40139ed9f0f7
letungbaoan <baoan7604@gmail.com>
2024-07-01 14:39:59 +0700

    Show data to form

--- src/pages/User/pages/Profile/Profile.tsx
+++ src/pages/User/pages/Profile/Profile.tsx
@@ -50,13 +50,17 @@ export default function Profile() {
     }
   }, [profile, setValue])
 
+  const onSubmit = handleSubmit(async (data) => {
+    // await updateProfileMutation.mutateAsync({data})
+  })
+
   return (
     <div className='rounded-sm bg-white px-2 pb-10 shadow md:px-7 md:pb-20'>
       <div className='border-b border-b-gray-200 py-6'>
         <h1 className='text-lg font-medium capitalize text-gray-900'>Hồ Sơ Của Tôi</h1>
         <div className='mt-1 text-sm text-gray-700'>Quản lý thông tin hồ sơ để bảo mật tài khoản</div>
       </div>
-      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start'>
+      <form className='mt-8 flex flex-col-reverse md:flex-row md:items-start' onSubmit={onSubmit}>
         <div className='mt-6 flex-grow md:mt-0 md:pr-12'>
           <div className='flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'>Email</div>
@@ -85,7 +89,7 @@ export default function Profile() {
                 render={({ field }) => (
                   <InputNumber
                     placeholder='Số điện thoại'
-                    errorMessage={errors.name?.message}
+                    errorMessage={errors.phone?.message}
                     classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
                     {...field}
                     onChange={field.onChange}
@@ -101,12 +105,18 @@ export default function Profile() {
                 register={register}
                 name='address'
                 placeholder='Địa chỉ'
-                errorMessage={errors.name?.message}
+                errorMessage={errors.address?.message}
                 classNameInput='w-full rounded-sm border border-gray-300 px-3 py-2 outline-none focus:border-gray-500 focus:shadow-sm'
               />
             </div>
           </div>
-        <DateSelect />
+          <Controller
+            control={control}
+            name='date_of_birth'
+            render={({ field }) => (
+              <DateSelect errorMessage={errors.date_of_birth?.message} value={field.value} onChange={field.onChange} />
+            )}
+          />
           <div className='mt-2 flex flex-col flex-wrap sm:flex-row'>
             <div className='truncate pt-3 capitalize sm:w-[20%] sm:text-right'></div>
             <div className='sm:w-[80%] sm:pl-5'>

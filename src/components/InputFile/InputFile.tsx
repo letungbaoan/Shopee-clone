351fad6384a7d285360b5f2b09baefff43ce582b
letungbaoan <baoan7604@gmail.com>
2024-07-10 00:29:24 +0700

    Active NavLink for UserSideNav

---
+++ src/components/InputFile/InputFile.tsx
@@ -0,0 +1,48 @@
+import { Fragment, useRef, useState } from 'react'
+import { toast } from 'react-toastify'
+import config from 'src/constants/config'
+
+interface Props {
+  onChange?: (file?: File ) => void
+}
+
+export default function InputFile({onChange} : Props) {
+  const fileInputRef = useRef<HTMLInputElement>(null)
+  const [file, setFile] = useState<File>()
+  const handleUpload = () => {
+    fileInputRef.current?.click()
+  }
+
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileFromLocal = event.target.files?.[0]
+    if (fileFromLocal && (fileFromLocal.size >= config.maxSizeUploadAvatar || !fileFromLocal?.type.includes('image'))) {
+      toast.error('Dụng lượng file tối đa 1 MB. Định dạng:.JPEG, .PNG', {
+        position: 'top-center'
+      })
+    } else {
+      onChange && onChange(fileFromLocal)
+    }
+  }
+
+  return (
+    <Fragment>
+    <input
+              className='hidden'
+              type='file'
+              accept='.jpg,.jpeg,.png'
+              ref={fileInputRef}
+              onChange={onFileChange}
+              onClick={(event) => {
+                ;(event.target as any).value = null
+              }}
+            />
+            <button
+              type='button'
+              className='flex h-10 items-center justify-end rounded-sm border bg-white px-6 text-sm text-gray-600 shadow-sm'
+              onClick={handleUpload}
+            >
+              Chọn ảnh
+            </button>
+    </Fragment>
+  )
+}

c05e8a7a0cd2acb446b81f37c919675098b7f891
letungbaoan <baoan7604@gmail.com>
2024-08-04 12:49:04 +0700

    Test not found page

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import matchers from '@testing-library/jest-dom/matchers'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter } from 'react-router-dom'
 
 expect.extend(matchers)
 
@@ -13,6 +13,7 @@ describe('App', () => {
       wrapper: BrowserRouter
     })
     const user = userEvent.setup()
+
     await waitFor(() => {
       expect(document.querySelector('title')?.textContent).toBe('Trang chủ | Shopee Clone')
     })
@@ -22,6 +23,17 @@ describe('App', () => {
       expect(screen.queryByText('Bạn chưa có tài khoản?')).toBeInTheDocument()
       expect(document.querySelector('title')?.textContent).toBe('Đăng nhập | Shopee Clone')
     })
-    screen.debug(document.body.parentElement as HTMLElement, 99999999)
+  })
+
+  test('Về trang not found', async () => {
+    const badRoute = '/some/bad/route'
+    render(
+      <MemoryRouter initialEntries={[badRoute]}>
+        <App />
+      </MemoryRouter>
+    )
+    await waitFor(() => {
+      expect(screen.getByText(/Page Not Found/i)).toBeInTheDocument()
+    })
   })
 })
